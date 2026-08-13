c0c8aab150f00ec91294b3218cc784e539b574f9
Antony <kiziff156@gmail.com>
2026-08-13 12:24:42 +0300

    test: add data base asserts in TestRequestCredit

---
+++ src/main/api/db/crud/credit_crud.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+from src.main.api.db.models.credit_table import Credit
+
+
+class CreditCrudDb:
+    @staticmethod
+    def get_credit_from_db_by_account_id(db:Session, account_id: int) -> Credit:
+        return db.query(Credit).filter_by(account_id=account_id).first()
+
+    @staticmethod
+    def get_credits_from_db_by_account_id(db:Session, account_id: int) -> list[Account]:
+        return db.query(Credit).filter_by(account_id=account_id).order_by(Credit.account_id).limit(2).all()
